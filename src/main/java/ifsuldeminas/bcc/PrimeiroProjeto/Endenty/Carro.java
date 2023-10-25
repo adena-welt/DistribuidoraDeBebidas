ef27cd70d00feeb4adbf9d7d94361824d2e95387
adena-welt <vchristina122@gmail.com>
2023-10-24 21:31:38 -0300

    Classe Carro

---
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Carro.java
@@ -0,0 +1,88 @@
+package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
+
+import java.util.List;
+
+public class Carro {
+    private Long id;
+    private String marca;
+    private String modelo;
+    private String placa;
+    private String cor;
+    private String ano;
+    private double quilometragem;
+    private Cliente cliente;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(double quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<OrdemServico> getOrdensServico() {
+        return ordensServico;
+    }
+
+    public void setOrdensServico(List<OrdemServico> ordensServico) {
+        this.ordensServico = ordensServico;
+    }
+
+    public Carro(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    private List<OrdemServico> ordensServico;
+}
