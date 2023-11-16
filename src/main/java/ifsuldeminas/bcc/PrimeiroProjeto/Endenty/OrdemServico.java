9b452f6a08927c0bf44a493acdf0a12eef280d15
GustavoHLM <127438798+GustavoHLM@users.noreply.github.com>
2023-11-16 19:51:05 -0300

    Update OrdemServico.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/OrdemServico.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/OrdemServico.java
@@ -1,79 +1,46 @@
-package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
+package ifsuldeminas.bcc.PrimeiroProjeto.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
 public class OrdemServico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String descricao;
-    private List<Funcionario> funcionario;
+
+    @ManyToMany
+    private List<Funcionario> funcionarios;
+
+    @ManyToOne
     private Cliente cliente;
+
+    @ManyToOne
     private Carro carro;
+
+    @ManyToMany
     private List<Pagamento> pagamentos;
+
+    @ManyToMany
     private List<Peca> pecas;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public List<Funcionario> getFuncionario() {
-        return funcionario;
-    }
-
-    public void setFuncionario(List<Funcionario> funcionario) {
-        this.funcionario = funcionario;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public Carro getCarro() {
-        return carro;
-    }
-
-    public void setCarro(Carro carro) {
-        this.carro = carro;
-    }
-
-    public List<Pagamento> getPagamentos() {
-        return pagamentos;
-    }
-
-    public void setPagamentos(List<Pagamento> pagamentos) {
-        this.pagamentos = pagamentos;
-    }
-
-    public List<Peca> getPecas() {
-        return pecas;
-    }
-
-    public void setPecas(List<Peca> pecas) {
-        this.pecas = pecas;
-    }
-
-    public OrdemServico(Long id, String descricao, List<Funcionario> funcionario, Cliente cliente, Carro carro, List<Pagamento> pagamentos, List<Peca> pecas) {
+    public OrdemServico(Long id, String descricao, List<Funcionario> funcionarios, Cliente cliente, Carro carro,
+                        List<Pagamento> pagamentos, List<Peca> pecas) {
         this.id = id;
         this.descricao = descricao;
-        this.funcionario = funcionario;
+        this.funcionarios = funcionarios;
         this.cliente = cliente;
         this.carro = carro;
         this.pagamentos = pagamentos;
         this.pecas = pecas;
     }
-
 }
