5b0f5cdda3a853720179288a65bc2f445321083e
bryan2772 <95505127+bryan2772@users.noreply.github.com>
2023-11-16 19:53:51 -0300

    Update Funcionario.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Funcionario.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Funcionario.java
@@ -1,50 +1,29 @@
-package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
+
+package ifsuldeminas.bcc.PrimeiroProjeto.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@PrimaryKeyJoinColumn(referencedColumnName = "ID")
+
 public class Funcionario extends Individuo {
-    public Long id;
-    public String nome;
-    public List<OrdemServico> ordensServico;
-    public DesempenhoFuncionario desempenho;
+    @OneToMany(mappedBy = "funcionarios")
+    private List<OrdemServico> ordensServico;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<OrdemServico> getOrdensServico() {
-        return ordensServico;
-    }
-
-    public void setOrdensServico(List<OrdemServico> ordensServico) {
-        this.ordensServico = ordensServico;
-    }
-
-    public DesempenhoFuncionario getDesempenho() {
-        return desempenho;
-    }
-
-    public void setDesempenho(DesempenhoFuncionario desempenho) {
-        this.desempenho = desempenho;
-    }
+    @OneToOne(mappedBy = "funcionario")
+    private DesempenhoFuncionario desempenho;
 
     public Funcionario(Long id, String nome, List<OrdemServico> ordensServico, DesempenhoFuncionario desempenho,
-                       long ID, String nomeIndividuo, String cpf, String telefone, String dataNascimento, String endereco) {
-        super(ID, nomeIndividuo, cpf, telefone, dataNascimento, endereco);
-        this.id = id;
-        this.nome = nome;
+                       Long ID, String cpf, String telefone, String dataNascimento, String endereco) {
+        super(ID, nome, cpf, telefone, dataNascimento, endereco);
         this.ordensServico = ordensServico;
         this.desempenho = desempenho;
     }
