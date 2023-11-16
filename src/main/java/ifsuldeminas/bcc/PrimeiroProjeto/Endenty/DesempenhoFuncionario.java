12b2ca27c61af71228f91f2e3659cb2d355c37d7
bryan2772 <95505127+bryan2772@users.noreply.github.com>
2023-11-16 19:51:25 -0300

    Update DesempenhoFuncionario.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/DesempenhoFuncionario.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/DesempenhoFuncionario.java
@@ -1,45 +1,24 @@
-package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
 
+
+package ifsuldeminas.bcc.PrimeiroProjeto.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
 public class DesempenhoFuncionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private double tempoMEdioReparo;
+
+    private double tempoMedioReparo;
     private double eficienciaPecas;
+
+    @OneToOne
     private Funcionario funcionario;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getTempoMEdioReparo() {
-        return tempoMEdioReparo;
-    }
-
-    public void setTempoMEdioReparo(double tempoMEdioReparo) {
-        this.tempoMEdioReparo = tempoMEdioReparo;
-    }
-
-    public double getEficienciaPecas() {
-        return eficienciaPecas;
-    }
-
-    public void setEficienciaPecas(double eficienciaPecas) {
-        this.eficienciaPecas = eficienciaPecas;
-    }
-
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
-
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
-
-
-    public DesempenhoFuncionario(Long id) {
-        this.id = id;
-    }
 }
