a70d785ef0e2523c5758560e579f7e8302707216
adena-welt <108073386+adena-welt@users.noreply.github.com>
2023-11-16 19:52:14 -0300

    Update AgendamentoAutomatico.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/AgendamentoAutomatico.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/AgendamentoAutomatico.java
@@ -1,46 +1,22 @@
-package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
 
+package ifsuldeminas.bcc.PrimeiroProjeto.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
 public class AgendamentoAutomatico {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
     private Cliente cliente;
+
     private String dataHora;
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public String getDataHora() {
-        return dataHora;
-    }
-
-    public void setDataHora(String dataHora) {
-        this.dataHora = dataHora;
-    }
-
-    public String getServico() {
-        return servico;
-    }
-
-    public void setServico(String servico) {
-        this.servico = servico;
-    }
-
-    public AgendamentoAutomatico(Long id) {
-        this.id = id;
-    }
-    private String servico;
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
 }
-
