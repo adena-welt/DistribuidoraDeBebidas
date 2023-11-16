2164a059b1350863011bf69af380626bbd44e326
GustavoHLM <127438798+GustavoHLM@users.noreply.github.com>
2023-11-16 19:55:46 -0300

    Update DistribuidoraDeBebidas.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/DistribuidoraDeBebidas.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/DistribuidoraDeBebidas.java
@@ -1,5 +1,12 @@
 package ifsuldeminas.bcc.PrimeiroProjeto;
 
-public class DistribuidoraDeBebidas {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Main {
+
+    public static void main(String args[]){
+        SpringApplication.run(Main.class, args);
+    }
 }
