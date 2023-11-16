23a87e8ee21402195812153e1af4c4f8df17223b
bryan2772 <95505127+bryan2772@users.noreply.github.com>
2023-11-16 19:53:11 -0300

    Update Filial.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Filial.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Filial.java
@@ -1,37 +1,21 @@
-package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
 
+package ifsuldeminas.bcc.PrimeiroProjeto.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
 public class Filial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nomeFilial;
     private String endereco;
     private String telefone;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNomeFilial() {
-        return nomeFilial;
-    }
-
-    public void setNomeFilial(String nomeFilial) {
-        this.nomeFilial = nomeFilial;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    
-  void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
 }
