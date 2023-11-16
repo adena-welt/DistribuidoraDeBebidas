fc862d158345de182f8cf30e84234a79da4db2c4
bryan2772 <95505127+bryan2772@users.noreply.github.com>
2023-11-16 19:54:26 -0300

    Update Individuo.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Individuo.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Individuo.java
@@ -1,20 +1,38 @@
-package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
+package ifsuldeminas.bcc.PrimeiroProjeto.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "ID")
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Individuo {
-    private long ID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
     private String nome;
     private String cpf;
     private String telefone;
     private String dataNascimento;
     private String endereco;
 
-    public Individuo(long id, String nome, String cpf, String telefone, String dataNascimento, String endereco) {
-        this.ID = id;
+    // Construtores, getters e setters...
+
+    public Individuo() {
+        // Construtor padrão
+    }
+
+    public Individuo(Long ID, String nome, String cpf, String telefone, String dataNascimento, String endereco) {
+        this.ID = ID;
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
         this.dataNascimento = dataNascimento;
         this.endereco = endereco;
     }
-
 }
