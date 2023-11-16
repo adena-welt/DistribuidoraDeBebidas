800685ad4eed77d726db2007ddb7e604a559b5c4
GustavoHLM <127438798+GustavoHLM@users.noreply.github.com>
2023-11-16 19:51:39 -0300

    Update Pagamento.java

--- src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Pagamento.java
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Endenty/Pagamento.java
@@ -1,49 +1,30 @@
-package ifsuldeminas.bcc.PrimeiroProjeto.Endenty;
+package ifsuldeminas.bcc.PrimeiroProjeto.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
 public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private double valor;
-    private String DataPagamento;
+
+    private String dataPagamento;
+
+    @ManyToOne
     private OrdemServico ordemServico;
 
-    public Pagamento(Long id, double valor, String DataPagamento, OrdemServico ordemServico) {
+    public Pagamento(Long id, double valor, String dataPagamento, OrdemServico ordemServico) {
         this.id = id;
         this.valor = valor;
-        this.DataPagamento = DataPagamento;
+        this.dataPagamento = dataPagamento;
         this.ordemServico = ordemServico;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public String getDataPagamento() {
-        return DataPagamento;
-    }
-
-    public void setDataPagamento(String DataPagamento) {
-        this.DataPagamento = DataPagamento;
-    }
-
-    public OrdemServico getOrdemServico() {
-        return ordemServico;
-    }
-
-    public void setOrdemServico(OrdemServico ordemServico) {
-        this.ordemServico = ordemServico;
-    }
-
 }
-
