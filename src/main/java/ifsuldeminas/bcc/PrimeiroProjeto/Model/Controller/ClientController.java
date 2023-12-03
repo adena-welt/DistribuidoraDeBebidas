98179f89d9e84467de46b113a3b7b563dffc6aec
GustavoHLM <127438798+GustavoHLM@users.noreply.github.com>
2023-12-03 19:52:45 -0300

    Controller Cliente

---
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Model/Controller/ClientController.java
@@ -0,0 +1,70 @@
+package ifsuldeminas.bcc.PrimeiroProjeto.Model.Controller;
+
+import ifsuldeminas.bcc.PrimeiroProjeto.Model.Entity.Cliente;
+import ifsuldeminas.bcc.PrimeiroProjeto.Model.Services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClientController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAllClientes() {
+        List<Cliente> clientes = clienteService.getAllClientes();
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getCliente(@PathVariable Long id) {
+        try {
+            Cliente cliente = clienteService.getClienteById(id);
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
+        try {
+            Cliente createdCliente = clienteService.createCliente(cliente);
+            return new ResponseEntity<>(createdCliente, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        try {
+            Cliente updatedCliente = clienteService.updateCliente(id, cliente);
+            return new ResponseEntity<>(updatedCliente, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
+        try {
+            clienteService.deleteCliente(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
