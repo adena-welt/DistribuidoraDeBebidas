be5293d3acf585ad5c347259faa01059e2defce9
bryan2772 <95505127+bryan2772@users.noreply.github.com>
2023-12-03 19:58:10 -0300

    controller funcionario

---
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Model/Controller/FuncionarioController.java
@@ -0,0 +1,70 @@
+package ifsuldeminas.bcc.PrimeiroProjeto.Model.Controller;
+
+import ifsuldeminas.bcc.PrimeiroProjeto.Model.Entity.Funcionario;
+import ifsuldeminas.bcc.PrimeiroProjeto.Model.Services.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Funcionario>> getAllFuncionarios() {
+        List<Funcionario> funcionarios = funcionarioService.getAllFuncionarios();
+        return new ResponseEntity<>(funcionarios, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Funcionario> getFuncionarioById(@PathVariable Long id) {
+        try {
+            Funcionario funcionario = funcionarioService.getFuncionarioById(id);
+            return new ResponseEntity<>(funcionario, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Funcionario> createFuncionario(@RequestBody Funcionario funcionario) {
+        try {
+            Funcionario createdFuncionario = funcionarioService.createFuncionario(funcionario);
+            return new ResponseEntity<>(createdFuncionario, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Funcionario> updateFuncionario(@PathVariable Long id, @RequestBody Funcionario funcionario) {
+        try {
+            Funcionario updatedFuncionario = funcionarioService.updateFuncionario(id, funcionario);
+            return new ResponseEntity<>(updatedFuncionario, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFuncionario(@PathVariable Long id) {
+        try {
+            funcionarioService.deleteFuncionario(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
