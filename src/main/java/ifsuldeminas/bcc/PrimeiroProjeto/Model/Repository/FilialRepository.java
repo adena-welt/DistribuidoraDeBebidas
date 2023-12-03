7d284a7fef658a401e806e9645894aa1c8f51a08
adena-welt <108073386+adena-welt@users.noreply.github.com>
2023-12-03 20:36:49 -0300

    repository filial

---
+++ src/main/java/ifsuldeminas/bcc/PrimeiroProjeto/Model/Repository/FilialRepository.java
@@ -0,0 +1,7 @@
+package ifsuldeminas.bcc.PrimeiroProjeto.Model.Repository;
+
+import ifsuldeminas.bcc.PrimeiroProjeto.Model.Entity.Filial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilialRepository extends JpaRepository<Filial, Long> {
+}
